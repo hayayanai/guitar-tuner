c61fc83b7d487c18ecbca09fa6e97663565470ad
hayayanai <hayayanai@users.noreply.github.com>
2025-12-09 00:41:26 +0900

    feat: スクロールバーを非表示にし、ボディのスタイルを改善

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from "vue";
 import "./styles.css";
 import App from "./App.vue";
 
+// 右クリック・コピー無効化
+document.addEventListener('contextmenu', e => e.preventDefault());
+document.addEventListener('copy', e => e.preventDefault());
+
 createApp(App).mount("#app");
